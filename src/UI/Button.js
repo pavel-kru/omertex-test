7ce3ba641874eb0be6e7c1f70715cfe7bca9fcb6
pavel-kru <pavlusha.kruglik@gmail.com>
2021-06-24 15:16:53 +0300

    Validation and updating logik
    Validation and updating logik .

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import classes from "./Button.module.scss";
+import styles from "./Button.module.scss";
 
 const Button = (props) => {
   return (
     <button
       disabled={props.disabled}
       onClick={props.onClick}
-      className={[classes.button, classes[props.type]].join(" ")}
+      className={[styles.button, styles[props.type]].join(" ")}
     >
       {props.children}
     </button>
--- src/containers/Form.js
+++ src/containers/Form.js
@@ -2,13 +2,14 @@ import React, { useReducer, useCallback } from "react";
 
 import Fieldset from "../components/Fieldset";
 import Button from "../UI/Button";
+import { ipValidation } from "../utils/check-validity";
 import styles from "./Form.module.scss";
 
 const formInitialState = {
   Ethernet: {
     ip: {
       auto: true,
-      adress: "",
+      address: "",
       subnetMask: "",
       defaultGateway: "",
     },
@@ -25,7 +26,7 @@ const formInitialState = {
     securityKey: "",
     ip: {
       auto: true,
-      adress: "",
+      address: "",
       subnetMask: "",
       defaultGateway: "",
     },
@@ -35,20 +36,63 @@ const formInitialState = {
       alternativeServer: "",
     },
   },
+  errors: {
+    Ethernet: { dns: {}, ip: {} },
+    Wireless: { dns: {}, ip: {} },
+  },
+};
+
+const checkValidityHandler = (obj) => {
+  return (
+    [...Object.values(obj.Ethernet), ...Object.values(obj.Wireless)].filter(
+      (err) => Object.values(err).length > 0
+    ).length === 0
+  );
 };
 
 const formReducer = (state, action) => {
   const { name, type, value } = action;
 
-  if (type=== 'SUBMIT' || type=== 'CANCEL') {
-    return formInitialState
+  if (type === "SUBMIT") {
+    console.log(
+      JSON.stringify({
+        Ethernet: { ...state.Ethernet },
+        Wireless: { ...state.Wireless },
+      })
+    );
+    return { ...formInitialState };
   }
-
+  if (type === "CANCEL") {
+    return { ...formInitialState };
+  }
 
   const updatePath = name && name.split(".");
 
   if (updatePath.length === 2 && (type === "checkbox" || type === "radio")) {
     const [path, key] = updatePath;
+    if (key === "enable" && state.Wireless.enable) {
+      return {
+        ...state,
+        Wireless: {
+          ...formInitialState.Wireless,
+        },
+        errors: {
+          ...state.errors,
+          Wireless: formInitialState.errors.Wireless,
+        },
+      };
+    }
+
+    if (key === "enableKey" && state.Wireless.enableKey) {
+      return {
+        ...state,
+        Wireless: {
+          ...state.Wireless,
+          enableKey: false,
+          securityKey: formInitialState.Wireless.securityKey,
+        },
+      };
+    }
     return {
       ...state,
       [path]: {
@@ -57,8 +101,28 @@ const formReducer = (state, action) => {
       },
     };
   }
+
   if (updatePath.length === 3 && (type === "checkbox" || type === "radio")) {
     const [path, subPath, key] = updatePath;
+    if (!state[path][subPath][key]) {
+      return {
+        ...state,
+        [path]: {
+          ...state[path],
+          [subPath]: {
+            ...formInitialState[path][subPath],
+          },
+        },
+        errors: {
+          ...state.errors,
+          [path]: {
+            ...state.errors[path],
+            [subPath]: "",
+          },
+        },
+      };
+    }
+
     return {
       ...state,
       [path]: {
@@ -70,6 +134,7 @@ const formReducer = (state, action) => {
       },
     };
   }
+
   if (updatePath.length === 2) {
     const [path, key] = updatePath;
     return {
@@ -91,35 +156,44 @@ const formReducer = (state, action) => {
           [key]: value,
         },
       },
+      errors: {
+        ...state.errors,
+        [path]: {
+          ...state.errors[path],
+          [subPath]: {
+            ...state.errors[path][subPath],
+            [key]: ipValidation(value, key),
+          },
+        },
+      },
     };
   }
-
-  return state;
+  return formInitialState;
 };
 
 function Form() {
   const [form, dispatchForm] = useReducer(formReducer, formInitialState);
 
-  const inputChangeHandler = useCallback(
-    ({ target }) => {
-      dispatchForm({
-        name: target.getAttribute("idx"),
-        type: target.type,
-        value: target.value,
-      });
-    },
-    []
-  );
+  const inputChangeHandler = useCallback(({ target }) => {
+    dispatchForm({
+      name: target.getAttribute("idx"),
+      type: target.type,
+      value: target.value,
+    });
+  }, []);
+
+  const formIsValid = checkValidityHandler(form.errors);
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    dispatchForm({type: 'SUBMIT'})
-  };
-  
-  const formCancelHandler = () => {
-    dispatchForm({type: 'CANCEL'})
+    if (!formIsValid) return;
+    dispatchForm({ type: "SUBMIT" });
   };
 
-
+  const formCancelHandler = (event) => {
+    event.preventDefault();
+    dispatchForm({ type: "CANCEL" });
+  };
 
   return (
     <form onSubmit={formSubmitHandler} className={styles.form}>
@@ -129,7 +203,9 @@ function Form() {
       </div>
       <div className={styles.buttons}>
         <Button type="submit">Save</Button>
-        <Button type="cancel" onClick={formCancelHandler}>Cancel</Button>
+        <Button type="cancel" onClick={formCancelHandler}>
+          Cancel
+        </Button>
       </div>
     </form>
   );
